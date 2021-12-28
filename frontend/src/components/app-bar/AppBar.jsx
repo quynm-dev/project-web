2f1fd8d5874727831aa8ec14a36e9b8758629bdd
nguyenminhquy25 <nmq25100002@gmail.com>
2021-12-28 19:38:22 +0700

    chore: style appbar

--- frontend/src/components/app-bar/AppBar.jsx
+++ frontend/src/components/app-bar/AppBar.jsx
@@ -1,23 +1,13 @@
 import * as React from 'react';
-import { Button, Toolbar, Box, AppBar, Typography } from '@mui/material';
+import { Toolbar, Box, AppBar } from '@mui/material';
 import PlagiarismOutlinedIcon from '@mui/icons-material/PlagiarismOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles({
-  appBarButton: {
-    '&:hover': {
-      color: '#f15e2c !important',
-    },
-  },
-});
+import { Link } from 'react-router-dom';
 
 export default function ButtonAppBar() {
-  const classes = useStyles();
-
   return (
     <Box>
       <AppBar position="static">
@@ -27,61 +17,96 @@ export default function ButtonAppBar() {
             justifyContent: 'flex-end',
           }}
         >
-          <Button
-            color="inherit"
-            sx={{ paddingRight: '20px', transition: 'all 0.5s' }}
-            className={classes.appBarButton}
-            href="/"
-          >
-            <PlagiarismOutlinedIcon />
-            <Typography sx={{ paddingLeft: '10px', fontSize: '12px' }}>
-              Tra cứu đơn hàng
-            </Typography>
-          </Button>
-          <Button
-            color="inherit"
-            sx={{ paddingRight: '20px', transition: 'all 0.5s' }}
-            className={classes.appBarButton}
-            href="/"
-          >
-            <LocationOnOutlinedIcon />
-            <Typography sx={{ paddingLeft: '10px', fontSize: '12px' }}>
-              Tìm cửa hàng
-            </Typography>
-          </Button>
-          <Button
-            color="inherit"
-            sx={{ paddingRight: '20px', transition: 'all 0.5s' }}
-            className={classes.appBarButton}
-            href="/shopping-cart"
-          >
-            <FavoriteBorderOutlinedIcon />
-            <Typography sx={{ paddingLeft: '10px', fontSize: '12px' }}>
-              Yêu thích
-            </Typography>
-          </Button>
-          <Button
-            color="inherit"
-            sx={{ paddingRight: '20px', transition: 'all 0.5s' }}
-            className={classes.appBarButton}
-            href="/login"
-          >
-            <PersonOutlineOutlinedIcon />
-            <Typography sx={{ paddingLeft: '10px', fontSize: '12px' }}>
-              Đăng nhập
-            </Typography>
-          </Button>
-          <Button
-            color="inherit"
-            sx={{ paddingRight: '20px', transition: 'all 0.5s' }}
-            className={classes.appBarButton}
-            href="/shopping-cart"
-          >
-            <ShoppingCartOutlinedIcon />
-            <Typography sx={{ paddingLeft: '10px', fontSize: '12px' }}>
-              Giỏ hàng (1)
-            </Typography>
-          </Button>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{ display: 'flex', alignItems: 'center', paddingX: '15px' }}
+            >
+              <Box>
+                <PlagiarismOutlinedIcon />
+              </Box>
+              <Box
+                sx={{
+                  paddingLeft: '10px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                }}
+              >
+                Tra cứu đơn hàng
+              </Box>
+            </Box>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{ display: 'flex', alignItems: 'center', paddingX: '15px' }}
+            >
+              <Box>
+                <LocationOnOutlinedIcon />
+              </Box>
+              <Box
+                sx={{
+                  paddingLeft: '10px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                }}
+              >
+                Tìm cửa hàng
+              </Box>
+            </Box>
+          </Link>
+          <Link to="/shopping-cart" style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{ display: 'flex', alignItems: 'center', paddingX: '15px' }}
+            >
+              <Box>
+                <FavoriteBorderOutlinedIcon />
+              </Box>
+              <Box
+                sx={{
+                  paddingLeft: '10px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                }}
+              >
+                Yêu thích
+              </Box>
+            </Box>
+          </Link>
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{ display: 'flex', alignItems: 'center', paddingX: '15px' }}
+            >
+              <Box>
+                <PersonOutlineOutlinedIcon />
+              </Box>
+              <Box
+                sx={{
+                  paddingLeft: '10px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                }}
+              >
+                Đăng nhập
+              </Box>
+            </Box>
+          </Link>
+          <Link to="/shopping-cart" style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{ display: 'flex', alignItems: 'center', paddingX: '15px' }}
+            >
+              <Box>
+                <ShoppingCartOutlinedIcon />
+              </Box>
+              <Box
+                sx={{
+                  paddingLeft: '10px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                }}
+              >
+                Giỏ hàng (1)
+              </Box>
+            </Box>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
